b063a21c35c3775fbe8c943b705a50f63f651d44
Igor Nikolaev <tamarisk60@yandex.ru>
2024-02-08 12:26:31 +0100

    firstCommit

---
+++ src/lesson_17/MainEmployee.java
@@ -0,0 +1,8 @@
+package lesson_17;
+
+public class MainEmployee {
+    public static void main(String[] args) {
+        Employee employee = new Employee("Vasya" , 21, 100000);
+        System.out.println(employee.toString());
+    }
+}
