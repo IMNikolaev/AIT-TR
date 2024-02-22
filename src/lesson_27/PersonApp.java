3c11ba2a89c1ff89d85c7b2dd91ee179cc2629c9
Igor Nikolaev <tamarisk60@yandex.ru>
2024-02-22 14:24:01 +0100

    22feb

--- src/lesson_27/PersonApp.java
+++ src/lesson_27/PersonApp.java
@@ -8,7 +8,6 @@ public class PersonApp {
     public static void main(String[] args) {
         Person person = new Person("email", "password");
         System.out.println(person);
-        System.out.println(person.isPassword("AbCdEfG1"));
     }
 
 
