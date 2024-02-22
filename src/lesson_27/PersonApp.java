eaa7b4438d89748ea49493a6cd86b63e5d607803
Igor Nikolaev <tamarisk60@yandex.ru>
2024-02-22 10:04:41 +0100

    20feb

---
+++ src/lesson_27/PersonApp.java
@@ -0,0 +1,16 @@
+package lesson_27;/*
+@date 21.02.2024
+@author Sergey Bugaienko
+*/
+
+public class PersonApp {
+
+    public static void main(String[] args) {
+        Person person = new Person("email", "password");
+        System.out.println(person);
+    }
+    // JUnit Jupiter/ TestNG
+
+
+
+}
